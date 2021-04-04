0c6a97e14a407200cbab7a9af69fbb8182e72f8a
dolphin323 <liza140620002@gmail.com>
2021-04-04 19:52:02 +0300

    Last ver

---
+++ src/Components/Data.js
@@ -0,0 +1,28 @@
+export const Data = [
+    {
+        question: "Can I use my solutions in my portfolio?",
+        answer:
+            'Of course! Frontend Mentor was created to make professionally designed projects more accessible to the development community.Please feel free to use anything you build in your portfolio.'
+    },
+    {
+        question: "Do I get a code review when I post my solution?",
+        answer: "If you would like to receive feedback please be sure to post a question when you submit your solution. The more specific you can be the better. This will give others in the community an idea of the help you're seeking."
+    },
+    {
+        question: "Can I cancel my subscription any time?",
+        answer: "Definitely! We provide easy account management through the Settings page. Click on the Billing tab and you'll be able to manage your subscription without a problem."
+    },
+    {
+        question: "Can I get my employer to pay for my subscription?",
+        answer: "If you're employed as a developer, your employer should be happy to pay for your subscription as part of your training budget. Be sure to ask them first."
+    },
+    {
+        question: "What if I change my mind after signing up as a PRO member?",
+        answer: `We offer a "fair use" refund policy within 14-days of starting your 
+     subscription. Fair use means that if you download one or two premium assets 
+     (designs, premium challenges) and decide it's not for you, we're happy to 
+     process a refund. After all, we want you to be satisfied with your purchase! 
+     But if you've downloaded three or more premium assets and then requested a 
+     refund, we reserve the right to say no. To request a refund, email 
+     ` }
+];
